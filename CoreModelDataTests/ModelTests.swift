c6dccfcc11e9e3c313ed891e3e06fcf1ed86d77e
Vosik <vosikvos@gmail.com>
2024-06-01 20:47:52 +0300

    t - add test Empty Contact and check its values

--- CoreModelDataTests/ModelTests.swift
+++ CoreModelDataTests/ModelTests.swift
@@ -6,9 +6,29 @@
 //
 
 import XCTest
+@testable import ElseCoreData
 
 final class ModelTests: XCTestCase {
 
-   
+    private var provider: CoreDataProvider!
+    
+    override func setUp() {
+        super.setUp()
+        provider = ContactsProvider()
+    }
+    
+    override func tearDown() {
+        provider = nil
+        super.tearDown()
+    }
+    
+    func test_checkDefaultValuesInNewContact_valuesShouldBeEmptyAndIsFavouriteIsFalse() {
+        let contact = Contact.empty(context: provider.context)
+        XCTAssertEqual(contact.name, "", "Expected name to be empty")
+        XCTAssertEqual(contact.phoneNumber, "", "Expected phone number to be empty")
+        XCTAssertEqual(contact.email, "", "Expected email to be empty")
+        XCTAssertFalse(contact.isFavourite, "Expected isFavourite to be false")
+        XCTAssertTrue(Calendar.current.isDateInToday(contact.dob), "Expected dob to be today")
+    }
 
 }
